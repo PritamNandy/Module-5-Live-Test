749a4941695b4be606dd0929f978ba9d7cc49e17
Prasun Nandy Pritam <pritamnandy84@yahoo.com>
2024-06-24 23:15:22 +0600

    adding comment

--- test.dart
+++ test.dart
@@ -1,3 +1,4 @@
+//MAIN CLASS
 void main() {
   var media = new Media();
   var song = new Song('Pritam');
@@ -6,12 +7,14 @@ void main() {
   song.play();
 }
 
+//BASE CLASS
 class Media {
   play() {
     print('Playing media...');
   }
 }
 
+//SUB/CHILD CLASS
 class Song extends Media {
   String artist = '';
   Song(this.artist);
